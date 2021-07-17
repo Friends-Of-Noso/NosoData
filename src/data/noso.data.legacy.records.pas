a4edb0d6071955b50f692f5faf40c34e9429f02c
Gustavo Carreno <guscarreno@gmail.com>
2021-07-17 03:04:35 +0100

    fix: Wrong lpi format

--- src/data/noso.data.legacy.records.pas
+++ src/data/noso.data.legacy.records.pas
@@ -16,14 +16,14 @@ type
     Number         : Int64;
     TimeStart      : Int64;
     TimeEnd        : Int64;
-    TimeTotal      : integer;
-    TimeLast20     : integer;
-    TrxTotales     : integer;
-    Difficult      : integer;
+    TimeTotal      : Integer;
+    TimeLast20     : Integer;
+    TrxTotales     : Integer;
+    Difficult      : Integer;
     TargetHash     : String[32];
     Solution       : String[200];
     LastBlockHash  : String[32];
-    NxtBlkDiff     : integer;
+    NxtBlkDiff     : Integer;
     AccountMiner   : String[40];
     MinerFee       : Int64;
     Reward         : Int64;
@@ -31,27 +31,27 @@ type
 
 { TLegacyOrderData }
   TLegacyOrderData = packed record
-    Block : integer;
-    OrderID : String[64];
+    Block      : integer;
+    OrderID    : String[64];
     OrderLines : Integer;
-    OrderType : String[6];
-    TimeStamp : Int64;
-    Concept : String[64];
-    TrxLine : integer;
-    Sender : String[120];
-    Address : String[40];
-    Receiver : String[40];
+    OrderType  : String[6];
+    TimeStamp  : Int64;
+    Concept    : String[64];
+    TrxLine    : integer;
+    Sender     : String[120];
+    Address    : String[40];
+    Receiver   : String[40];
     AmmountFee : Int64;
     AmmountTrf : Int64;
-    Signature : String[120];
-    TrfrID : String[64];
+    Signature  : String[120];
+    TrfrID     : String[64];
   end;
 
 { TLegacyMyTrxData }
   TLegacyMyTrxData = packed record
-    block : integer;
-    time  : int64;
-    tipo  : string[6];
+    block    : integer;
+    time     : int64;
+    tipo     : string[6];
     receiver : string[64];
     monto    : int64;
     trfrID   : string[64];
