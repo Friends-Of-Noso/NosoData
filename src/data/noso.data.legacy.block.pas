284932f8ba9a95a3091a560d746570fa8f635464
Gustavo Carreno <guscarreno@gmail.com>
2021-07-17 01:00:49 +0100

    feat: TLegacyBlock.Create and test

--- src/data/noso.data.legacy.block.pas
+++ src/data/noso.data.legacy.block.pas
@@ -10,13 +10,79 @@ uses
 ;
 
 type
+{ TString32 }
+  TString32 = String[32];
+
+{ TString40 }
+  TString40 = String[40];
+
+{ TString200 }
+  TString200 = String[200];
+
 { TLegacyBlock }
   TLegacyBlock = class(TObject)
   private
+    FNumber: Int64;
+    FTimeStart: Int64;
+    FTimeEnd: Int64;
+    FTimeTotal: Integer;
+    FTimeLast20: Integer;
+    FTransfers: Integer;
+    FDifficulty: Integer;
+    FTargetHash: TString32;
+    FSolution: TString200;
+    FLastBlockHash: TString32;
+    FNextBlockDifficulty: Integer;
+    FMinerAddress: TString40;
+    FFee: Int64;
+    FReward: Int64;
   protected
   public
     constructor Create;
     destructor Destroy; override;
+
+    property Number: Int64
+      read FNumber
+      write FNumber;
+    property TimeStart: Int64
+      read FTimeStart
+      write FTimeStart;
+    property TimeEnd: Int64
+      read FTimeEnd
+      write FTimeEnd;
+    property TimeTotal: Integer
+      read FTimeTotal
+      write FTimeTotal;
+    property TimeLast20: Integer
+      read FTimeLast20
+      write FTimeLast20;
+    property Transfers: Integer
+      read FTransfers
+      write FTransfers;
+    property Difficulty: Integer
+      read FDifficulty
+      write FDifficulty;
+    property TargetHash: TString32
+      read FTargetHash
+      write FTargetHash;
+    property Solution: TString200
+      read FSolution
+      write FSolution;
+    property LastBlockHash: TString32
+      read FLastBlockHash
+      write FLastBlockHash;
+    property NextBlockDifficulty: Integer
+      read FNextBlockDifficulty
+      write FNextBlockDifficulty;
+    property Miner: TString40
+      read FMinerAddress
+      write FMinerAddress;
+    property Fee: Int64
+      read FFee
+      write FFee;
+    property Reward: Int64
+      read FReward
+      write FReward;
   published
   end;
 
@@ -26,12 +92,24 @@ implementation
 
 constructor TLegacyBlock.Create;
 begin
-  //
+  FNumber:= -1;
+  FTimeStart:= -1;
+  FTimeEnd:= -1;
+  FTimeTotal:= -1;
+  FTimeLast20:= -1;
+  FTransfers:= 0;
+  FDifficulty:= -1;
+  FTargetHash:= EmptyStr;
+  FSolution:= EmptyStr;
+  FLastBlockHash:= EmptyStr;
+  FNextBlockDifficulty:= -1;
+  FMinerAddress:= EmptyStr;
+  FFee:= 0;
+  FReward:= 0;
 end;
 
 destructor TLegacyBlock.Destroy;
 begin
-  //
   inherited Destroy;
 end;
 
--- tests/data/testnosodatalegacyblock.pas
+++ tests/data/testnosodatalegacyblock.pas
@@ -32,7 +32,20 @@ implementation
 
 procedure TTestNosoDataLegacyBlock.CheckFieldsCreate;
 begin
-
+  AssertEquals('Block Number is -1', -1, FLegacyBlock.Number);
+  AssertEquals('Block TimeStart is -1', -1, FLegacyBlock.TimeStart);
+  AssertEquals('Block TimeEnd is -1', -1, FLegacyBlock.TimeEnd);
+  AssertEquals('Block TimeTotal is -1', -1, FLegacyBlock.TimeTotal);
+  AssertEquals('Block TimeLast20 is -1', -1, FLegacyBlock.TimeLast20);
+  AssertEquals('Block TrxTotales count is 0', 0, FLegacyBlock.Transfers);
+  AssertEquals('Block Difficulty is -1', -1, FLegacyBlock.Difficulty);
+  AssertEquals('Block TargetHash is empty', EmptyStr, FLegacyBlock.TargetHash);
+  AssertEquals('Block Solution is empty', EmptyStr, FLegacyBlock.Solution);
+  AssertEquals('Block LastBlockHash is empty', EmptyStr, FLegacyBlock.LastBlockHash);
+  AssertEquals('Block NextBlockDifficulty is -1', -1, FLegacyBlock.NextBlockDifficulty);
+  AssertEquals('Block Miner is empty', EmptyStr, FLegacyBlock.Miner);
+  AssertEquals('Block Fee is 0', 0, FLegacyBlock.Fee);
+  AssertEquals('Block Reward is 0', 0, FLegacyBlock.Reward);
 end;
 
 procedure TTestNosoDataLegacyBlock.TestNosoDataLegacyBlockCreate;
