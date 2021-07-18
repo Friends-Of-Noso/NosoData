9943535c0dcf329ec0921c96c7efad68db5fe2b1
Gustavo Carreno <guscarreno@gmail.com>
2021-07-18 07:03:51 +0100

    feat: Package created

---
+++ packages/nosodata.pas
@@ -0,0 +1,17 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit NosoData;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  Noso.Data.Block, Noso.Data.Blocks, Noso.Data.Operation, 
+  Noso.Data.Operations, Noso.Data.Legacy.Block, Noso.Data.Legacy.Transaction, 
+  Noso.Data.Legacy.Transactions, Noso.Data.Legacy.Types;
+
+implementation
+
+end.
